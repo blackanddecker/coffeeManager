668ea9b970bfe39b75137608a9bef7bd172286c4
Vangelis Foufikos <baggelisfoufikos@yahoo.com>
2020-01-06 22:45:19 +0200

    working android

--- shopManager/app/src/main/java/com/example/shopmanager/MainActivity.java
+++ shopManager/app/src/main/java/com/example/shopmanager/MainActivity.java
@@ -8,39 +8,51 @@ import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
 import android.os.Bundle;
+
 import android.view.View;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import org.json.JSONObject;
 
-
 public class MainActivity extends AppCompatActivity {
+    public static Context mainActivityContext;
+
+    static String postUrl = "http://192.168.1.5:5000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        AndroidNetworking.initialize(getApplicationContext());
 
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.INTERNET}, 0);
+
+        mainActivityContext = this;
     }
 
-
-    public void select_product(View V){
-        String url = "http://127.0.0.1:5000/select_product";
-        AndroidNetworking.get("http://10.0.2.2:8080/select_product")
-                .setPriority(Priority.MEDIUM)
-                .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        System.out.println(response.toString());
-
-                    }
-                    @Override
-                    public void onError(ANError error) {
-                        System.out.println(error);
-                    }
-                });
-
+    public void login(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, 2);
     }
 
+    public void register(View v) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView responseText = findViewById(R.id.responseText);
+        if (requestCode == 2) { // Login
+            responseText.setText("Successful Login.");
+        } else {
+            responseText.setText("Invalid or no data entered. Please try again.");
+        }
+    }
 }
