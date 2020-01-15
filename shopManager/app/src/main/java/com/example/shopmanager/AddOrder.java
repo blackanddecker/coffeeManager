cfe1559ed3b839ab860671397c21617703501a3a
Vangelis Foufikos <baggelisfoufikos@yahoo.com>
2020-01-16 00:53:07 +0200

    add Menu after login with data!

---
+++ shopManager/app/src/main/java/com/example/shopmanager/AddOrder.java
@@ -0,0 +1,13 @@
+package com.example.shopmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_order);
+    }
+}
